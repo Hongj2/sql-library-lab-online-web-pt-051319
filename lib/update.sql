ed6d266db9b714441e4f2629d46af8571b297e48
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-24 23:26:19 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
+
 UPDATE characters SET species = 'Martian' WHERE species = 'cylon' 
-=======
+
 "UPDATE characters SET name = 'Martin' WHERE name = 'Character Four' "
->>>>>>> 31c16a172af66124147b6cf3a2097792bed7b320
+
